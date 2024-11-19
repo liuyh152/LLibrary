047254e40e01ae41e2691775624972ec639a85ca
liuyh152 <liuyh0805>
2024-11-20 00:13:39 +0800

    fix

--- app/src/main/java/com/liuyh/library/frame/adapter/com/liuyh/library/example/MainActivity.java
+++ app/src/main/java/com/liuyh/library/frame/adapter/com/liuyh/library/example/MainActivity.java
@@ -1,10 +1,19 @@
 package com.liuyh.library.frame.adapter.com.liuyh.library.example;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.liuyh.library.R;
 
 /**
  * Created by liuyuhang on 2016/11/10.
  */
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adapter_loading_layout);
+    }
 }
